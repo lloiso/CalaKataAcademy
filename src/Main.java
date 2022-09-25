2928e618b2dc777236e25e188bf5b9dc3568af9a
Dmitrii <lisp.tnt@gmail.com>
2022-09-25 17:23:45 +0300

    преобразователь из арабских в римские цифры переделан до 100 + преобразователь из римских в арабские до 10

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("11");
+        System.out.println(56 % 10);
         String as = "8 / 2";
 
 //        calc(as);
         System.out.println(calc(as));
-        for (int i = 1; i < 19; i++) {
+        for (int i = 1; i < 99; i++) {
             String tmp = Integer.toString(i);
-            System.out.println(handlerArabianNumberToRomeNumber(tmp));
+            System.out.println(handlerArabianNumberToRomeNumber(tmp) + " " + i);
         }
+
+        System.out.println(handlerRomeNumberToArabianNumber("I"));
+        System.out.println(handlerRomeNumberToArabianNumber("II"));
+        System.out.println(handlerRomeNumberToArabianNumber("III"));
+        System.out.println(handlerRomeNumberToArabianNumber("IV"));
+        System.out.println(handlerRomeNumberToArabianNumber("V"));
+        System.out.println(handlerRomeNumberToArabianNumber("VI"));
+        System.out.println(handlerRomeNumberToArabianNumber("VII"));
+        System.out.println(handlerRomeNumberToArabianNumber("VIII"));
+        System.out.println(handlerRomeNumberToArabianNumber("IX"));
+        System.out.println(handlerRomeNumberToArabianNumber("X"));
     }
 
     public static String calc(String input) {
@@ -38,13 +49,37 @@ public class Main {
     }
 
     public static String handlerArabianNumberToRomeNumber(String incoming) {
-        String romeNum = "I";
-        int num = Integer.parseInt(incoming);
+        String romeNum = "";
+        String romeDecimalNum = "";
+        int num = Integer.parseInt(incoming) % 10;
+        int decimalNum = Integer.parseInt(incoming) / 10;
+
+        if (decimalNum > 0 && decimalNum < 4) {
+            romeDecimalNum = "X";
+            for (int i = 1; i < decimalNum; i++) {
+                romeDecimalNum = romeDecimalNum + "X";
+            }
+        } else if (decimalNum == 4) {
+            romeDecimalNum = "X";
+            romeDecimalNum = romeDecimalNum + "L";
+        } else if (decimalNum > 4 && decimalNum < 9) {
+            romeDecimalNum = "L";
+            for (int i = 5; i < decimalNum; i++) {
+                romeDecimalNum = romeDecimalNum + "X";
+            }
+        } else if (decimalNum == 9) {
+            romeDecimalNum = "X";
+            romeDecimalNum = romeDecimalNum + "C";
+        }
+
+
         if (num > 0 && num < 4) {
+            romeNum = "I";
             for (int i = 1; i < num; i++) {
                 romeNum = romeNum + "I";
             }
         } else if (num == 4) {
+            romeNum = "I";
             romeNum = romeNum + "V";
         } else if (num > 4 && num < 9) {
             romeNum = "V";
@@ -52,23 +87,40 @@ public class Main {
                 romeNum = romeNum + "I";
             }
         } else if (num == 9) {
+            romeNum = "I";
             romeNum = romeNum + "X";
-        } else if (num > 9 && num < 14) {
-            romeNum = "X";
-            for (int i = 10; i < num; i++) {
-                romeNum = romeNum + "I";
-            }
-        }else if (num == 14){
-            romeNum = "XIV";
-        }else if (num > 14 && num < 19){
-            romeNum = "XV";
-            for (int i = 15; i < num; i++) {
-                romeNum = romeNum + "I";
-            }
         }
-        return romeNum;
+        return romeDecimalNum + romeNum;
     }
+
     public static String handlerRomeNumberToArabianNumber(String incoming) {
-        return "1";
+        int result = 0;
+
+        switch (incoming.charAt(0)) {
+            case 'I':
+                result = 1;
+                for (int i = 1; i < incoming.length(); i++) {
+                    if (incoming.charAt(i) == 'I') {
+                        result++;
+                    } else if (incoming.charAt(i) == 'V') {
+                        result = 4;
+                    } else if (incoming.charAt(i) == 'X') {
+                        result = 9;
+                    }
+                }
+                break;
+            case 'V':
+                result = 5;
+                for (int i = 1; i < incoming.length(); i++) {
+                    if (incoming.charAt(i) == 'I') {
+                        result++;
+                    }
+                }
+                break;
+            case 'X':
+                result = 10;
+                break;
+        }
+        return Integer.toString(result);
     }
 }
